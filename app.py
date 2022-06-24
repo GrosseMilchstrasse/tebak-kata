cc652771de5fba13b88d89bcb7d91b670ae322c8
leggoss <akbar@bww.com>
2022-06-24 22:30:56 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['post'])
+def my_form_post():
+    text = request.form['text']
+    angka = request.form['angka']
+    processed_text = text.lower()
+    processed_number = int(angka)
+
+    import pandas as pd
+
+    #Load Data Kata
+    df = pd.read_csv('data_kata.csv')
+    df = df.drop(labels="kelas", axis=1)
+    df = df.drop(labels="type", axis=1)
+    df = df.astype(str)
+
+    list_kata_df = df['lema'].tolist()
+    list_kata = [o.lower() for o in list_kata_df]
+
+    #Program
+    import itertools
+    from itertools import permutations, chain
+
+    #inp = input("Masukan huruf-huruf yang ingin disusun: ")
+    inp_kata = processed_text
+    lst = []
+    kata = [x.lower() for x in inp_kata]
+
+    #r = int(input("Jumlah huruf tiap kata: "))
+    inp_jumlah_huruf = processed_number
+
+    for y in list(chain.from_iterable(permutations(kata,i) for i in range (2,processed_number+1))):
+        z=("".join(y))
+        if len(z)>2:
+            lst.append(z)
+
+    list_hasil = [t for t in list_kata if t in lst]
+
+    tiga_kata=[a for a in list_hasil if len(a)==3]
+    empat_kata=[b for b in list_hasil if len(b)==4]
+    lima_kata=[c for c in list_hasil if len(c)==5]
+    enam_kata=[d for d in list_hasil if len(d)==6]
+    tujuh_kata=[e for e in list_hasil if len(e)==7]
+
+    #print([t for t in listkata if t in lst])
+    string_hasil = " ".join(str(u) for u in list_hasil)
+
+    return render_template('index2.html', list_hasil=list_hasil, 
+        tiga_kata=tiga_kata, empat_kata=empat_kata, lima_kata=lima_kata, enam_kata=enam_kata, tujuh_kata=tujuh_kata)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#env\Scripts\activate.ps1
+#$env:FLASK_APP = ".\app.py"
+#$env:FLASK_ENV = "development"
